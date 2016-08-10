4e335eced5bdabb5ee65daf6611aaf9ad88b5920
Fitzzp <zhanpeng@hey900.com>
2016-08-10 15:21:15 +0800

    hot fix

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -32,7 +32,7 @@ class Snippet(models.Model):
         """
         lexer = get_lexer_by_name(self.language)
         linenos = self.linenos and 'table' or False
-        options = self.tltle and {'title': self.title} or {}
+        options = self.title and {'title': self.title} or {}
         formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
         self.highlighted = highlight(self.code, lexer, formatter)
         super(Snippet, self).save(*args, **kwargs)
