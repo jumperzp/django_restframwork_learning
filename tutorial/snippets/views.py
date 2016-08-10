65a275a6542e6d3f6ebaa3eb527a475bdc7fb1a7
Fitzzp <zhanpeng@hey900.com>
2016-08-10 14:29:45 +0800

    Tutorial 4: Authentication and Permissions

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,19 +1,41 @@
+from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer, SnippetSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
     """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or detele a snippet instance.
     """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
